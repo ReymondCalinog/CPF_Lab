9e2f4c70d13d1e59446f52ac221dfcc9ea78cfe1
ReymondCalinog <reymondcalinog@lpulaguna.edu.ph>
2021-10-13 13:17:44 +0800

    OE-4

--- OE-4.cpp
+++ OE-4.cpp
@@ -1,6 +1,6 @@
 //Name: Reymond Y. Calinog
 //Activity: OE-4
-//Description: For loop program that creates a "Pyramid" or Triangle from giza
+//Description: Post card from gizah, a for loop program that creates a "Pyramid" or Triangle from giza
 //Date: 10/13/2021
 
 #include <iostream>
@@ -10,14 +10,22 @@ using namespace std;
 int main(){
     int n, a, b, c=1;
 
+    /* 
+    The varibales and their purposes:
+    "n" - it is used for the value inputted for the number of rows
+    "a" - it is used for value of spaces of the rows
+    "b" - it is used for the value of columns to build it structure using asterisk
+    "c" - is the same as for the "b" variable, it will replicate the other side of "b"
+    */
+
     cout << "Input the height of the pyramid: ";
     cin >> n;
 
-    for (a = 1; a <=n ; a++){
+    for (a = 1; a <= n ; a++){
         for (b = 1; b <= n-a; b++){
             cout << "  ";
         }
-        for (b=1; b<= c; b++){
+        for (b = 1; b <= c; b++){
             if (b == 1 || a == n || b == c){
                 cout << "*" << " ";
             }
